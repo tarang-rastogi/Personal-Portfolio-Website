ade827de0326f842c502398186c5c831797df923
tarang-rastogi <tarangrastogi9@gmail.com>
2026-04-28 10:25:54 +0530

    Add new features

---
+++ script.js
@@ -0,0 +1,12 @@
+let tabLinks=document.getElementsByClassName('tab-links');
+    let tabContents=document.getElementsByClassName('tab-contents');
+    function opentab(tabname){
+        for(tablink of tabLinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabContents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add('active-link')
+        document.getElementById(tabname).classList.add('active-tab')
+    }
