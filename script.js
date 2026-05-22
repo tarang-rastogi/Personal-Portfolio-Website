0ab97502726afbb856bdf6a5785db46f7c679994
tarang-rastogi <tarangrastogi9@gmail.com>
2026-05-22 17:47:20 +0530

    Complete mobile UI

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let tabLinks=document.getElementsByClassName('tab-links');
-    let tabContents=document.getElementsByClassName('tab-contents');
-    function opentab(tabname){
-        for(tablink of tabLinks){
-            tablink.classList.remove("active-link");
-        }
-        for(tabcontent of tabContents){
-            tabcontent.classList.remove("active-tab");
-        }
-        event.currentTarget.classList.add('active-link')
-        document.getElementById(tabname).classList.add('active-tab')
+let tabLinks = document.getElementsByClassName('tab-links');
+let tabContents = document.getElementsByClassName('tab-contents');
+function opentab(tabname) {
+    for (tablink of tabLinks) {
+        tablink.classList.remove("active-link");
     }
+    for (tabcontent of tabContents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add('active-link')
+    document.getElementById(tabname).classList.add('active-tab')
+}
+
+let sidemeu = document.getElementById('sidemenu');
+function openmenu() {
+    sidemeu.style.right = '0';
+}
+function closemenu() {
+    sidemeu.style.right = '-200px'
+}
